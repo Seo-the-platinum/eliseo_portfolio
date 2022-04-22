8b79abc3aab7eba050b8083fe9e0a48b957b5aed
Eliseo <eliseoflores@eliseos-mini.attlocal.net>
2022-04-22 15:48:29 -0700

    switched browserrouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from './App';
 import { Provider } from 'react-redux'
 import store from './app/store'
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
