290b5c26a2f8ec2d1e1445d1f790084ea32b2592
Eliseo <eliseoflores@eliseos-mini.attlocal.net>
2022-04-24 00:23:58 -0700

    changed about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
         Eliseo Flores
       </h1>
       <p className='aboutBody'> 
-        Ex telecommunications field agent looking to help the planet utilizing software engineering.
+        Ex telecommunications field agent looking to make an impact in the world utilizing his skills in software engineering..
         He sets his sights on his next challenge, becoming a full stack engineer, and seeks to increase his frontend and backend stats.
       </p>
       <p className='aboutBody'>
         Although young, our hero is confident, humble, dependable, and has a strong work ethic.
         With multiple projects and nano degrees under his belt, our hero embarks on a new journey. 
-        Eager to find a new organization where he can push past his limits and be a helpful member.
+        He is eager to find a new organization where he can push his limits, learn, and contribute as a respected member of a team..
       </p>
     </div>
   )
