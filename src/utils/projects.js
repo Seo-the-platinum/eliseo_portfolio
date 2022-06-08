819386da3f923ac00b1154bde1eb6cc4422499c7
Eliseo <eliseoflores@eliseos-mini.attlocal.net>
2022-06-08 12:15:11 -0700

    changed projects order

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,4 +1,11 @@
 export const Projects_Data = [
+    {
+        description: 'E commerce site built with react, redux, and firebase. Uses firebase authorization, firestore, and firebase bucket.Items can be added and removed from cart and when the user checksout, items get added to order history',
+        name: 'Bee Bee Barry Demo',
+        github: 'https://github.com/Seo-the-platinum/ecommerce-app',
+        site: 'https://629f9549bc424e00566c90f6--charming-dasik-fbd20d.netlify.app/',
+        source: 'https://media.giphy.com/media/rO4Q05UdAkhSvQEcHu/giphy.gif'
+    },
     { 
         description: 'Black Jack game built using vanilla JavaScript',
         name: 'Black Jack Clone',
@@ -13,11 +20,4 @@ export const Projects_Data = [
         site: 'https://seo-the-platinum.github.io/chess-app/',
         source: 'https://media.giphy.com/media/tW6dcVtWaCBRcJp0QW/giphy.gif',
     },
-    {
-        description: 'E commerce site built with react, redux, and firebase. Uses firebase authorization, firestore, and firebase bucket.Items can be added and removed from cart and when the user checksout, items get added to order history',
-        name: 'Bee Bee Barry Demo',
-        github: 'https://github.com/Seo-the-platinum/ecommerce-app',
-        site: 'https://629f9549bc424e00566c90f6--charming-dasik-fbd20d.netlify.app/',
-        source: 'https://media.giphy.com/media/rO4Q05UdAkhSvQEcHu/giphy.gif'
-    }
 ]
